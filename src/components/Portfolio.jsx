a9e1ad206a3c7e37ddd937662be599027e367a15
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-23 05:10:46 +0000

    Feat: finished About Page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
+import weather from "../assets/portfolio/weather.png";
+import installNode from "../assets/portfolio/installNode.jpg";
+import navbar from "../assets/portfolio/navbar.jpg";
+import linkedInClone from "../assets/portfolio/linkedInClone.png";
+import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
+    const portfolios = [
+        {
+          id: 1,
+          src: weather,
+          title:'Weather App',
+          demo:'https://dynamic-babka-49f2f4.netlify.app/',
+          code: 'https://github.com/alb1nut/reactJs-weather-app'
+        },
+        {
+          id: 2,
+          src: linkedInClone,
+          title:'LinkedIn Clone',
+          demo:'',
+          code:''
+        },
+        {
+          id: 3,
+          src: navbar,
+          title:'Weather App',
+          demo:'',
+          code:''
+        },
+        {
+          id: 4,
+          src: reactSmooth,
+          title:'Weather App',
+          demo:'',
+          code:''
+        },
+     
+      ];
   return (
-    <div>
-      
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src ,title,demo ,code}) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+               <p className="py-4 mx-5 ">{title}</p>
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href={demo}>Demo</a>
+                  
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a href={code}>Code</a>
+                  
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
