a2c7f663096a5d2901a729704efa29dddf9e2b8a
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-23 06:21:32 +0000

    Modify:finished  modifying about  page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,16 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="pt-5 pl-5 ml-10 w-full h-screen bg-gradient-to-b from-white-800 to-gray text-black"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+        <div className="pb-3 mt-20">
+          <p className=" text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
+        <p className="text-xl  mt-20">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
           deserunt illum mollitia officiis qui exercitationem perferendis neque
           quasi a recusandae necessitatibus tempora iusto! Blanditiis error
@@ -24,13 +24,13 @@ const About = () => {
 
         <br />
 
-        <p className="text-xl">
+        <p className="text-xl mb-20">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
           pariatur, vel similique sint, nobis aspernatur ut praesentium
           explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
+          {/* quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
           Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+          magni quo! Eum cupiditate debitis labore. */}
         </p>
       </div>
     </div>
