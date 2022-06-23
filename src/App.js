9863f0861da0da9b211bc947343695338593f5eb
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-23 04:18:03 +0000

    Feat: created home and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
-    <div className="text-4xl font-bold font-signature">
-     <h1>Hello</h1>
+    <div >
+    <NavBar />
+    <Home />
+    <SocialLinks />
     </div>
   );
 }
