033bebfda28eb3954e9537c94665e9c49730e9ad
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-23 06:02:01 +0000

    Modify:finished  modifying

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,9 +31,9 @@ const NavBar = () => {
     ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-black bg-white fixed">
+    <div className="  flex justify-between items-center w-full h-20 px-4 text-black bg-white fixed ">
     <div>
-      <h1 className="text-5xl font-signature ml-2">Albert Nutifafa Dovlo</h1>
+      <h1 className="text-3xl lg:text-5xl font-signature ml-2">Albert Nutifafa Dovlo</h1>
     </div>
 
     <ul className="hidden md:flex">
@@ -57,7 +57,7 @@ const NavBar = () => {
     </div>
 
     {nav && (
-      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-white to-gray-800 text-gray-500">
         {links.map(({ id, link }) => (
           <li
             key={id}
