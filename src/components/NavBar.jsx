266002ee94a8e1d2c870bd11e77e2a1e514caba3
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-23 07:13:55 +0000

    Modify: changed navbar mobile screen color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -57,7 +57,7 @@ const NavBar = () => {
     </div>
 
     {nav && (
-      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-white to-gray-800 text-gray-500">
+      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-white to-gray-100 text-black">
         {links.map(({ id, link }) => (
           <li
             key={id}
