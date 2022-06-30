25fb22f6c06235cf8ec12c4381c4e4d427a797ec
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-30 07:55:18 +0000

    adjusted margin top of experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -70,7 +70,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="mt-80 md:mt-1 mb-80 bg-gradient-to-b from-gray-100 to-white w-full h-screen"
+      className="mt-80 md:mt-52 mb-80 bg-gradient-to-b from-gray-100 to-white w-full h-screen"
     >
       <div className=" max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-black">
         <div className='ml-20 mt-80'>
