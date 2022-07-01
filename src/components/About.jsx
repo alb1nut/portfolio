fe7cb1d6206e9c55934e4b60cb41415c372439a7
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-01 12:38:12 +0000

    added margin to overlapping components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="pt-5 pl-5 ml-10 w-full h-screen bg-gradient-to-b from-white-800 to-gray text-black"
+      className=" mb-52 pt-5 pl-5 ml-10 w-full h-screen bg-gradient-to-b from-white-800 to-gray text-black"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-3 mt-20">
@@ -13,7 +13,7 @@ const About = () => {
           </p>
         </div>
 
-        <p className="text-xl  mt-5">
+        <p className="text-lg mr-10 md:text-xl  mt-5">
           Hi, my name is Albert and i am a web developer who's paionate about all
           things JavaScript.I'm a developer because i'm inspired by the idea of
           being a better me today than i was yesterday.Put simply, I love to
@@ -22,15 +22,15 @@ const About = () => {
 
         <br />
 
-        <p className="text-xl mb-5">
+        <p className="text-lg mr-10 md:text-xl mb-5">
           I am currently opened for  opportunities to work on Frontend
           application and ReactJs project.
         </p>
-        <p className="text-xl mb-5">
+        <p className="text-lg mr-10 md:text-xl mb-5">
           When I'm not busy building apps, you can find me playing/watching
           basketball ,playing Chess or reading a good book.
         </p>
-        <p className="text-xl mb-20">
+        <p className="text-lg mr-10 md:text-xl mb-20">
           If you will like to learn more about me ,please visit my contact
           section
         </p>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     name="contact"
     className=" w-full h-screen bg-gradient-to-b from-white to-gray-100 p-4 text-black"
   >
-    <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+    <div className="mt-auto flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
       <div className="mt-80 md:mt-1 ml-20 pb-8">
         <p className="text-4xl font-bold inline border-b-4 border-gray-500">
           Cotact
