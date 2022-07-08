adee1a6e24036b74fd963f89c000df0a83580b59
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-08 06:35:58 +0000

    updated portfolioprojects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,50 +1,48 @@
-import React from 'react'
+import React from "react";
 import weather from "../assets/portfolio/weather.png";
-import linkedInClone from "../assets/portfolio/linkedInClone.JPG";
+import troski from "../assets/portfolio/troski.png";
 import task from "../assets/portfolio/Task.JPG";
 import portfolio from "../assets/portfolio/portfolio.JPG";
 import Technical from "../assets/portfolio/Technical.JPG";
 
-
 const Portfolio = () => {
-    const portfolios = [
-        {
-          id: 1,
-          src: weather,
-          title:'Weather App',
-          demo:'https://dynamic-babka-49f2f4.netlify.app/',
-          code: 'https://github.com/alb1nut/reactJs-weather-app'
-        },
-        {
-          id: 2,
-          src: linkedInClone,
-          title:'LinkedIn Clone',
-          demo:'',
-          code:'https://github.com/alb1nut/linkedin-clone'
-        },
-        {
-          id: 3,
-          src: task,
-          title:'Task Manager App',
-          demo:'https://chipper-heliotrope-878bce.netlify.app/',
-          code:'https://github.com/alb1nut/Task-Manager-App/tree/main/Desktop/Web%20Development/1.a.%20React%20Projects/blogging'
-        },
-        {
-          id: 4,
-          src: portfolio,
-          title: 'Portfolio',
-          demo:'https://albertdovlo.com/',
-          code:'https://github.com/alb1nut/portfolio'
-        },
-        {
-          id: 5,
-          src: Technical,
-          title:'Technical Documentation Page',
-          demo:'https://alb1nut.github.io/Technical-Documentation-Page/',
-          code:'https://github.com/alb1nut/Technical-Documentation-Page'
-        },
-     
-      ];
+  const portfolios = [
+    {
+      id: 1,
+      src: troski,
+      title: "Troski-dashboard | A driver bus management app",
+      demo: "https://famous-cassata-c8ea59.netlify.app/",
+      code: "https://github.com/alb1nut/troski-dashboard",
+    },
+    {
+      id: 2,
+      src: weather,
+      title: "Weather App",
+      demo: "https://dynamic-babka-49f2f4.netlify.app/",
+      code: "https://github.com/alb1nut/reactJs-weather-app",
+    },
+    {
+      id: 3,
+      src: task,
+      title: "Task Manager App",
+      demo: "https://chipper-heliotrope-878bce.netlify.app/",
+      code: "https://github.com/alb1nut/Task-Manager-App/tree/main/Desktop/Web%20Development/1.a.%20React%20Projects/blogging",
+    },
+    {
+      id: 4,
+      src: portfolio,
+      title: "Portfolio",
+      demo: "https://albertdovlo.com/",
+      code: "https://github.com/alb1nut/portfolio",
+    },
+    {
+      id: 5,
+      src: Technical,
+      title: "Technical Documentation Page",
+      demo: "https://alb1nut.github.io/Technical-Documentation-Page/",
+      code: "https://github.com/alb1nut/Technical-Documentation-Page",
+    },
+  ];
   return (
     <div
       name="portfolio"
@@ -59,22 +57,20 @@ const Portfolio = () => {
         </div>
 
         <div className="m-auto w-3/4 grid sm:grid-cols-2 md:grid-cols-2 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src ,title,demo ,code}) => (
+          {portfolios.map(({ id, src, title, demo, code }) => (
             <div key={id} className="shadow-lg shadow-black rounded-lg">
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-               <p className="py-4 mx-5 ">{title}</p>
+              <p className="py-4 mx-5 ">{title}</p>
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   <a href={demo}>Demo</a>
-                  
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                <a href={code}>Code</a>
-                  
+                  <a href={code}>Code</a>
                 </button>
               </div>
             </div>
@@ -82,7 +78,7 @@ const Portfolio = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
